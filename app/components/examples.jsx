c3daa82ee44482e5ade45fa04e2b0133d77e3858
Joel Darling <joel.darling@gmail.com>
2016-08-06 13:00:56 -0400

    added grid system & updated examples page

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,8 +1,20 @@
 const React = require('react');
+const {Link} = require('react-router');
 
 const Examples = () => {
   return (
-    <h3> Examples </h3>
+    <div>
+      <h1 className="text-center"> Examples </h1>
+      <p> Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=New York'> New York, NY </Link>
+        </li>
+        <li>
+          <Link to='/?location=Boston'> Boston, MA </Link>
+        </li>
+      </ol>
+    </div>
   );
 }
 module.exports = Examples;
